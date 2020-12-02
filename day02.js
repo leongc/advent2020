20545893a32f984022a37c1dcaf26f05894f3923
Cheng Leong <leongc@alumni.rice.edu>
2020-12-01 23:38:07 -0600

    Day 2 part 2

--- day02.js
+++ day02.js
@@ -1040,3 +1040,32 @@ var day02input = [
 ];
 
 console.log(day02input.filter(isValid).length);
+
+/*
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
+*/
+
+function isValid2(inputLine) {
+  var [policy, password] = inputLine.split(':', 2);
+  var [range, letter] = policy.split(' ');
+  var [low, high] = range.split('-');
+  return !!((password[parseInt(low)] === letter) ^ (password[parseInt(high)] === letter)); // convert binary xor into boolean
+}
+
+console.assert(isValid2("1-3 a: abcde"));
+console.assert(!isValid2("1-3 b: cdefg"));
+console.assert(!isValid2("2-9 c: ccccccccc"));
+
+console.log(day02input.filter(isValid2).length);
