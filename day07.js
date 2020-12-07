bc9ab7d58d9bcf9546120604a26d2d0b052e218a
Cheng Leong <leongc@alumni.rice.edu>
2020-12-07 01:25:41 -0600

    Day 7 part 2

--- day07.js
+++ day07.js
@@ -691,3 +691,79 @@ var day07input = [
 'dark plum bags contain 5 shiny olive bags, 1 drab violet bag, 2 striped black bags, 1 faded coral bag.',
 ];
 console.log(goldBagColors(day07input).size);
+
+/*
+--- Part Two ---
+It's getting pretty expensive to fly these days - not because of ticket prices, but because of the ridiculous number of bags you need to buy!
+
+Consider again your shiny gold bag and the rules from the above example:
+
+faded blue bags contain 0 other bags.
+dotted black bags contain 0 other bags.
+vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+
+Of course, the actual rules have a small chance of going several levels deeper than this example; be sure to count all of the bags, even if the nesting becomes topologically impractical!
+
+Here's another example:
+
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+In this example, a single shiny gold bag must contain 126 other bags.
+
+How many individual bags are required inside your single shiny gold bag?
+*/
+function getContainerContentMap(input) {
+  var containerContentMap = new Map(); // container => Map(color, qty)
+  for (var i = 0; i < input.length; i++) {
+    var [container, contents] = input[i].split(' bags contain ', 2);
+    if (contents == 'no other bags.') {
+      continue;
+    }
+    var contentMap = new Map();
+    contents.replaceAll(/ bags?\.?/g, '').split(', ').forEach( qtyColor => { 
+        var [ignore, color] = qtyColor.split(/\d+ /, 2);
+        var qty = parseInt(qtyColor);
+        contentMap.set(color, qty);
+      } );
+    containerContentMap.set(container, contentMap);
+  }
+  return containerContentMap;
+}
+
+function getSize(color, containerContentMap, bagSizes = new Map()) {
+  if (bagSizes.has(color)) {
+      return bagSizes.get(color);
+  }
+  var contentMap = containerContentMap.get(color);
+  if (contentMap === undefined) {
+    return 0;
+  }
+  var count = Array.from(contentMap.entries()).reduce( (count, [key, value]) => { 
+      return count + value + (value * getSize(key, containerContentMap, bagSizes));
+  }, 0);
+  bagSizes.set(color, count);
+  return count;
+}
+var testCCM = getContainerContentMap(day07test);
+console.assert(getSize('dark olive', testCCM) === 7);
+console.assert(getSize('shiny gold', testCCM) === 32);
+
+var day07test2 = [
+  'shiny gold bags contain 2 dark red bags.',
+'dark red bags contain 2 dark orange bags.',
+'dark orange bags contain 2 dark yellow bags.',
+'dark yellow bags contain 2 dark green bags.',
+'dark green bags contain 2 dark blue bags.',
+'dark blue bags contain 2 dark violet bags.',
+'dark violet bags contain no other bags.',
+];
+console.assert(getSize('shiny gold', getContainerContentMap(day07test2)) === 126);
+
+console.log(getSize('shiny gold', getContainerContentMap(day07input)));
