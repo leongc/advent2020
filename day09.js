7f849bc073942e3b4a307f84e81c410adc30b6a2
Cheng Leong <leongc@alumni.rice.edu>
2020-12-10 00:35:13 -0600

    Day 9 part 2

--- day09.js
+++ day09.js
@@ -1093,4 +1093,59 @@ var day09input = [
 54699510342400,
 97367744515158,
 ];
-console.log(not2sum(25, day09input));
+var part1 = not2sum(25, day09input);
+console.log(part1);
+
+/*
+--- Part Two ---
+The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
+
+Again consider the above example:
+
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+
+To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
+
+What is the encryption weakness in your XMAS-encrypted list of numbers?
+*/
+function rangeSum(target, input) {
+  for (var i = input.indexOf(target) - 1; i>1; i--) {
+    var sum = input[i];
+    // console.log('reset', sum);
+    for (var j = i - 1; j>0; j--) {
+      sum += input[j];
+      // console.log(input[j], sum);
+      if (sum === target) {
+        var range = input.slice(j, i+1);
+        // console.log(range);
+        return Math.min(...range) + Math.max(...range);
+      }
+      if (sum > target) {
+        break;
+      }
+    }
+  }
+}
+console.assert(rangeSum(127, day09test) === 62);
+
+console.log(rangeSum(part1, day09input));
