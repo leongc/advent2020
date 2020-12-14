e2e3d0a9929c550c7e613775d9c9cfe7eae8466c
Cheng Leong <leongc@alumni.rice.edu>
2020-12-14 03:13:26 -0600

    Day 14 part 2

--- day14.js
+++ day14.js
@@ -651,3 +651,121 @@ var day14input = [
 'mem[39996] = 2787',
 ];
 console.log(sum(day14input));
+
+/*
+--- Part Two ---
+For some reason, the sea port's computer system still can't communicate with your ferry's docking program. It must be using version 2 of the decoder chip!
+
+A version 2 decoder chip doesn't modify the values being written at all. Instead, it acts as a memory address decoder. Immediately before a value is written to memory, each bit in the bitmask modifies the corresponding bit of the destination memory address in the following way:
+
+If the bitmask bit is 0, the corresponding memory address bit is unchanged.
+If the bitmask bit is 1, the corresponding memory address bit is overwritten with 1.
+If the bitmask bit is X, the corresponding memory address bit is floating.
+A floating bit is not connected to anything and instead fluctuates unpredictably. In practice, this means the floating bits will take on all possible values, potentially causing many memory addresses to be written all at once!
+
+For example, consider the following program:
+
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+When this program goes to write to memory address 42, it first applies the bitmask:
+
+address: 000000000000000000000000000000101010  (decimal 42)
+mask:    000000000000000000000000000000X1001X
+result:  000000000000000000000000000000X1101X
+After applying the mask, four bits are overwritten, three of which are different, and two of which are floating. Floating bits take on every possible combination of values; with two floating bits, four actual memory addresses are written:
+
+000000000000000000000000000000011010  (decimal 26)
+000000000000000000000000000000011011  (decimal 27)
+000000000000000000000000000000111010  (decimal 58)
+000000000000000000000000000000111011  (decimal 59)
+Next, the program is about to write to memory address 26 with a different bitmask:
+
+address: 000000000000000000000000000000011010  (decimal 26)
+mask:    00000000000000000000000000000000X0XX
+result:  00000000000000000000000000000001X0XX
+This results in an address with three floating bits, causing writes to eight memory addresses:
+
+000000000000000000000000000000010000  (decimal 16)
+000000000000000000000000000000010001  (decimal 17)
+000000000000000000000000000000010010  (decimal 18)
+000000000000000000000000000000010011  (decimal 19)
+000000000000000000000000000000011000  (decimal 24)
+000000000000000000000000000000011001  (decimal 25)
+000000000000000000000000000000011010  (decimal 26)
+000000000000000000000000000000011011  (decimal 27)
+The entire 36-bit address space still begins initialized to the value 0 at every address, and you still need the sum of all values left in memory at the end of the program. In this example, the sum is 208.
+
+Execute the initialization program using an emulator for a version 2 decoder chip. What is the sum of all values left in memory after it completes?
+*/
+
+function makeMask2(maskStr) {
+  return {
+    makeGenerator: function*(dec) {
+      var valStr = dec.toString(2).padStart(maskStr.length, '0');
+      var stack = []
+      var i = 0;
+      var prefix = "";
+      while (true) {
+        if (i === maskStr.length) {
+          yield prefix;
+          // next, take alternate path using 1
+          i = stack.pop();
+          if (i === undefined) {
+            // no more branches
+            return;
+          }
+          prefix = prefix.substring(0, i) + '1';
+        } else {
+          switch (maskStr[i]) {
+            case '0': prefix += valStr[i]; break;
+            case '1': prefix += '1'; break;
+            case 'X': prefix += '0'; stack.push(i); break; // branch later
+          }
+        }
+        i++;
+      } // wend
+    } // makeGenerator
+  };
+} // makeMask2
+var testMask2 = makeMask2('000000000000000000000000000000X1001X');
+var gen42 = testMask2.makeGenerator(42);
+console.assert(gen42.next().value === '000000000000000000000000000000011010');
+console.assert(gen42.next().value === '000000000000000000000000000000011011');
+console.assert(gen42.next().value === '000000000000000000000000000000111010');
+console.assert(gen42.next().value === '000000000000000000000000000000111011');
+
+function sum2(input) {
+  var mem = new Map();
+  var mask;
+  for (const line of input) {
+    var [opStr, valStr] = line.split(' = ', 2);
+    if (opStr === 'mask') {
+      mask = makeMask2(valStr);
+    } else { // mem[addr]
+      var addr = parseInt(opStr.substring(4), 10); // skip 'mem['
+      var val = parseInt(valStr, 10 /* decimal */);
+      for (const astr of mask.makeGenerator(addr)) {
+        var adec = parseInt(astr, 2 /* binary */);
+        mem.set(adec, val);
+        // console.log(astr, adec, val);
+      }
+    }
+  }
+  var result = 0;
+  for (const v of mem.values()) {
+    result += v;
+  }
+  return result;
+}
+
+var day14test2 = [
+'mask = 000000000000000000000000000000X1001X',
+'mem[42] = 100',
+'mask = 00000000000000000000000000000000X0XX',
+'mem[26] = 1',
+];
+console.assert(sum2(day14test2) === 208);
+
+console.log(sum2(day14input));
